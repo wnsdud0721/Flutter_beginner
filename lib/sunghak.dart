82c90940ff4b46c22e783af407e6b67113d7126b
wnsdud0721 <wnsdud0721@naver.com>
2023-07-11 00:47:10 +0900

    개인페이지 파일만들기 완료

---
+++ lib/sunghak.dart
@@ -0,0 +1,17 @@
+// 성학's 자기소개 페이지
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Sunghak extends StatelessWidget {
+  Sunghak({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("Sunghak's page"),
+      ),
+    );
+  }
+}
