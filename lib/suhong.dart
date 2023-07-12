b3657d7258095ef04938b0491616029c46536dda
박수홍 <parksuhong12@naver.com>
2023-07-12 10:46:56 +0900

    애러나는 코드 수정

--- lib/suhong.dart
+++ lib/suhong.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Suhong extends StatelessWidget {
+  const Suhong({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
